54bbb3204d95f4f5ad6a550b5218e4bf3752153d
dv0gt <dominik.vogt123@gmail.com>
2023-03-01 17:26:12 +0100

    feat: build file

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BINARY_NAME=kustomize-graph-1.0
+
+env GOOS=windows GOARCH=amd64 go build -o ${BINARY_NAME}-windows.exe ./cmd/main.go
+env GOOS=linux GOARCH=amd64 go build -o ${BINARY_NAME}-linux ./cmd/main.go
+
