8287709a7fc8d90b989f52486ee4b7284dd1c15a
dv0gt <dominik.vogt123@gmail.com>
2023-03-10 19:57:58 +0100

    feat: add recursion

--- pkg/kustomizationgraph/kustomizationgraph.go
+++ pkg/kustomizationgraph/kustomizationgraph.go
@@ -2,7 +2,9 @@ package kustomizationgraph
 
 import (
 	"fmt"
+	"hash/fnv"
 	"path/filepath"
+	"strconv"
 
 	"github.com/dv0gt/kustomize-graph-md/pkg/models"
 	"github.com/pkg/errors"
@@ -24,40 +26,53 @@ func NewGraph(kustomizeContext KustomizationContext) *KustomizationGraph {
 
 func (g *KustomizationGraph) BuildGraph(entryPath string) (string, error) {
 
-	file, err := g.kustomizationContext.GetFromDirectory(entryPath)
-
-	if err != nil {
-		return "", errors.Wrapf(err, "Unable to get kustomization file from given directory %v", entryPath)
-	}
-
 	markdown := "```mermaid"
 	markdown += "\nflowchart TD"
-	markdown += "\nsubgraph " + filepath.Base(entryPath)
-	markdown += "\nP0{{kustomization.yaml}}"
 
-	for i, p := range file.PatchesStrategicMerge {
-		markdown += "\nP0 --> |patchesStrategicMerge| PATCH" + fmt.Sprint(i) + "(" + p + ")"
+	subgraph, err := g.addSubGraph(entryPath, filepath.Base(entryPath))
+	if err != nil {
+		return "", err
 	}
-
-	for i, r := range file.Resources {
-		file, err = g.kustomizationContext.GetFromDirectory(entryPath + "/" + r)
-
-		if file != nil { // directory with kustomization.yaml
-			markdown += "\nsubgraph " + r
-			markdown += "\ndirection TB"
-			markdown += "\nRESOURCE" + fmt.Sprint(i) + "{{kustomization.yaml}}"
-
-			// TODO: recursive ...
-
-			markdown += "\nend"
-
-			markdown += "\nP0 --> |resources| " + r
-		}
-
-	}
-
-	markdown += "\nend"
+	markdown += subgraph
 	markdown += "\n```"
 
 	return markdown, nil
+}
+
+func (g *KustomizationGraph) addSubGraph(directory string, subGraphName string) (string, error) {
+	start := "K" + hash(directory)
+	markdown := "\nsubgraph " + subGraphName
+	markdown += "\n" + start + "{{kustomization.yaml}}"
+
+	file, err := g.kustomizationContext.GetFromDirectory(directory)
+	if err != nil {
+		return "", errors.Wrapf(err, "Unable to get kustomization file from given directory %v", directory)
+	}
+
+	for i, p := range file.PatchesStrategicMerge {
+		markdown += "\n" + start + " --> |patchesStrategicMerge| " + start + "P" + fmt.Sprint(i) + "(" + p + ")"
+	}
+
+	for _, r := range file.Resources {
+		resourcePath := directory + "/" + r
+		file, _ = g.kustomizationContext.GetFromDirectory(resourcePath)
+
+		if file != nil { // directory with kustomization.yaml
+			subgraph, err := g.addSubGraph(resourcePath, r)
+			if err != nil {
+				return "", err
+			}
+			markdown += subgraph
+			markdown += "\n" + start + " --> |resources| " + r
+		}
+	}
+	markdown += "\nend"
+
+	return markdown, err
+}
+
+func hash(s string) string {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return strconv.FormatUint(uint64(h.Sum32()), 10)
 }
