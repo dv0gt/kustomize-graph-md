8592ee727468905a3368b98100157d9d26ac4107
dv0gt <info.dominik.vogt+dv0gt@gmail.com>
2023-10-10 15:55:40 +0200

    fix: build path

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 BINARY_NAME=kustomize-markdown
 
 mkdir -p ./bin/
-env GOOS=darwin GOARCH=amd64 go build -o ./bin/${BINARY_NAME}-darwin-amd64 ./cmd/main.go
-env GOOS=linux GOARCH=amd64 go build -o ./bin/${BINARY_NAME}-linux-amd64 ./cmd/main.go
+env GOOS=darwin GOARCH=amd64 go build -o ./bin/${BINARY_NAME}-darwin-amd64 ./main.go
+env GOOS=linux GOARCH=amd64 go build -o ./bin/${BINARY_NAME}-linux-amd64 ./main.go
 
